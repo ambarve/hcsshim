9cddf5cbe04debf6941d767b91ec1fdb8a81466c
Kevin Parsons <kevpar@microsoft.com>
2020-04-13 22:16:50 +0000

    Include branch name in version

--- version.sh
+++ version.sh
@@ -5,7 +5,8 @@ set -eux
 cd /source
 
 gitVersion=$(git describe --match 'v[0-9]*' --always --long --tags)
+branch=$(git branch --show-current)
 timeStamp=$(date --utc +%Y%m%d)
-version=cdpx-${gitVersion}-${timeStamp}-${CDP_DEFINITION_BUILD_COUNT}
+version=cdpx-${gitVersion}+${branch}.${timeStamp}.${CDP_DEFINITION_BUILD_COUNT}
 
 echo '##vso[build.updatebuildnumber]'$version
