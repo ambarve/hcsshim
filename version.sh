36ed68e60357ff194dfc288095da956809fdb46d
Kevin Parsons <kevpar@microsoft.com>
2020-04-13 23:29:33 +0000

    Updated version.sh

--- version.sh
+++ version.sh
@@ -5,8 +5,7 @@ set -eux
 cd /source
 
 gitVersion=$(git describe --match 'v[0-9]*' --always --long --tags)
-branch=$(git branch --show-current)
 timeStamp=$(date --utc +%Y%m%d)
-version=cdpx-${gitVersion}+${branch}.${timeStamp}.${CDP_DEFINITION_BUILD_COUNT}
+version=cdpx-${gitVersion}+${BUILD_SOURCEBRANCH////_}.${timeStamp}.${CDP_DEFINITION_BUILD_COUNT}
 
 echo '##vso[build.updatebuildnumber]'$version
